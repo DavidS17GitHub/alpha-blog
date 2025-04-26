4d8baa3820a1bca7da23ccc89cc9fb3a6d92c099
David Salamanca <davidsalamanca96@outlook.com>
2025-04-25 19:31:50 -0500

    Create controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,3 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 end
-
-def home
-end
-
-def about
-end
